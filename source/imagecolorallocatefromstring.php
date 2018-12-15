5e5af2a119232c6e5b16d4c10c6af4efcb0a790b
andrewgjohnson <andrew@andrewgjohnson.com>
2018-12-15 17:52:00 -0600

    v1.0.1

--- source/imagecolorallocatefromstring.php
+++ source/imagecolorallocatefromstring.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Imagecolorallocatefromstring v1.0.0
+ * Imagecolorallocatefromstring v1.0.1
  *
  * Copyright (c) 2018 Andrew G. Johnson <andrew@andrewgjohnson.com>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
